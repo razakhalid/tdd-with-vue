6c25da2f9b71bb67e2e425d3b78619ef475fcd88
razakhalid <ra97za@gmail.com>
2023-04-15 12:43:43 -0500

    refactored tests with lifecycle hooks

--- tests/unit/SignupPage.spec.js
+++ tests/unit/SignupPage.spec.js
@@ -58,13 +58,32 @@ describe('Sign Up Page', () => {
    });
    describe('Interactions', () => {
 
+      // setup server
+      let reqBody;
+      let clickCounter = 0;
+      const server = setupServer(
+          rest.post("/api/1.0/users", (req, res, ctx) => {
+             reqBody = req.body;
+             clickCounter += 1;
+             return res(ctx.status(200));
+          })
+      );
+
+      beforeAll(() => server.listen());
+      beforeEach(() => {
+         clickCounter = 0;
+         server.resetHandlers();
+      })
+      afterAll(() => server.close());
+
+      let button;
       const setup = async function () {
          render(SignupPage);
          const usernameInput = screen.queryByLabelText("Username");
          const emailInput = screen.queryByLabelText("E-mail");
          const passwordInput = screen.queryByLabelText("Password");
          const passwordRepeatInput = screen.queryByLabelText("Password Repeat");
-
+         button = screen.queryByRole("button", { name: "Sign Up"});
          await userEvent.type(usernameInput, "raza");
          await userEvent.type(emailInput, "raza@gmail.com");
          await userEvent.type(passwordInput, "P4ssword");
@@ -72,38 +91,26 @@ describe('Sign Up Page', () => {
       }
 
       it('enables the button when requirements are met', async () => {
-         render(SignupPage);
+         await setup();
          const passwordInput = screen.queryByLabelText("Password");
          const passwordRepeatInput = screen.queryByLabelText("Password Repeat");
-         const button = screen.queryByRole("button", { name: "Sign Up"});
-
          await userEvent.type(passwordInput, "P4ssword");
          await userEvent.type(passwordRepeatInput, "P4ssword");
          expect(button).toBeEnabled();
       });
       it( 'sends username, email and password to backend on btn click', async () => {
 
-         // setup server
-         let reqBody;
-         const server = setupServer(
-            rest.post("/api/1.0/users", (req, res, ctx) => {
-               reqBody = req.body;
-               return res(ctx.status(200));
-            })
-         );
-         server.listen();
-
-
          await setup();
-         const button = screen.queryByRole("button", { name: "Sign Up"});
+
 
          // const mockFn = jest.fn();
          // axios.post = mockFn;
          // window.fetch = mockFn;
 
          await userEvent.click(button);
+         await screen.findByText("Please check your email to activate your account");
 
-         await server.close();
+
 
          // const firstCall = mockFn.mock.calls[0];
          // const body = firstCall[1];
@@ -117,24 +124,12 @@ describe('Sign Up Page', () => {
       });
       it(  'does not allow btn click when api call in progress', async () => {
 
-         // setup server
-         let clickCounter = 0;
-         const server = setupServer(
-             rest.post("/api/1.0/users", (req, res, ctx) => {
-                clickCounter += 1;
-                return res(ctx.status(200));
-             })
-         );
-         server.listen();
-
          await setup();
 
-         const button = screen.queryByRole("button", { name: "Sign Up"});
+
 
          await userEvent.click(button);
          await userEvent.click(button);
-
-         await server.close();
 
          expect(clickCounter).toBe(1);
       });
@@ -148,20 +143,14 @@ describe('Sign Up Page', () => {
       });
       it(  'displays account activation info after signup', async () => {
 
-         // setup server
-         const server = setupServer(
-             rest.post("/api/1.0/users", (req, res, ctx) => {
-                return res(ctx.status(200));
-             })
-         );
-         server.listen();
+
 
          await setup();
 
-         const button = screen.queryByRole("button", { name: "Sign Up"});
+
 
          await userEvent.click(button);
-         await server.close();
+
 
          const text = await screen.findByText("Please check your email to activate your account");
          expect(text).toBeVisible();
@@ -173,44 +162,34 @@ describe('Sign Up Page', () => {
          expect(text).not.toBeVisible();
       });
       it(  'hides account activation info after failing signup', async () => {
-         // setup server
-         const server = setupServer(
+
+         server.use(
              rest.post("/api/1.0/users", (req, res, ctx) => {
-                // return res(ctx.status(400));
+                return res(ctx.status(400));
              })
          );
-         server.listen();
 
          await setup();
 
-         const button = screen.queryByRole("button", { name: "Sign Up"});
+
          await userEvent.click(button);
-         await server.close();
+
 
          const text = screen.queryByText("Please check your email to activate your account");
 
          expect(text).not.toBeVisible();
       });
-      it(  'hides signup form after signup', async () => {
-         // setup server
-         const server = setupServer(
-             rest.post("/api/1.0/users", (req, res, ctx) => {
-                return res(ctx.status(200));
-             })
-         );
-         server.listen();
+      xit(  'hides signup form after signup', async () => {
 
          await setup();
-
-         const button = screen.queryByRole("button", { name: "Sign Up"});
          const form = screen.queryByTestId("signup-form");
 
          await userEvent.click(button);
-         await server.close();
 
          await waitFor(() => {
-            expect(form).not.toBeVisible();
+            expect(form).not.toBeInTheDocument();
          });
       });
+
    });
 });
